ba3efdd8252cdf03f2fb4bcecaabc0700d08c0d1
Raf Ferlin <Raf@fixthemix.be>
2019-12-23 12:29:14 +0100

    services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
 
 
 @Component({
@@ -9,12 +11,17 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes;
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
+
 
   ngOnInit() {
+    this.getHeroes();
+  }
+  getHeroes():void{
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
